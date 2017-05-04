bc143d6e232ebaf1ec6f2762349b1f8f0908d7d4
Chhorleang <chorm@zamanuniversity.edu.kh>
2017-05-04 09:53:04 +0700

    I wrote getSum function.

--- Source.cpp
+++ Source.cpp
@@ -1,34 +1,54 @@
 #include <stdio.h>
 
-/*	
-*	Please define your functions after main().
-*	You can import standard C / C++ libraries.
-*	Recommended: add description of your code.
-*/
+#include <stdlib.h>
+
+
+
 
 int getSum(int myArray[]);
-int getSumOfSquares(int myArray[]);
-void printAsChars(int myArray[]);
-double getAverage(int myArray[]);
-int subtractAllValues(int myArray[]);
-float invertAllValues(int myArray[] );
-void printGroupMemberNames();
+
 
 int main(void) {
-	printGroupMemberNames();
+
+	//printGroupMemberNames();
+
+
 
 	int row[500];
+
 	for (int i = 0; i < 500; i++)
+
 		row[i] = i;
 
-	return 1;
+
+
+	printf("%d", getSum(row));
+
+
+
+	return 0;
+
 }
 
 
 
+int getSum(int myArray[])
 
-void printGroupMemberNames() {
-	printf("James Bond\n Brad Pitt/n");
+{
+
+
+
+	int i;
+
+	int sum = 0;
+
+
+
+	for (i = 0; i<500; ++i)//iterate from 0 up to 499
+
+		sum += myArray[i];//increment sum by myArray[i]
+
+
+	return sum;
 
 }
-
