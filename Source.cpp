55598c5f08dde41c62fc0741b17539b23a9a8972
Sotheng <schheang2@zamanuniversity.edu.kh>
2017-05-04 10:01:49 -0700

    done!!

--- Source.cpp
+++ Source.cpp
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /*
 *	Please define your functions after main().
@@ -6,12 +6,12 @@
 *	Recommended: add description of your code.
 */
 
-//int getSum(int myArray[]);
-//int getSumOfSquares(int myArray[]);
-//void printAsChars(int myArray[]);
+int getSum(int myArray[]);
+int getSumOfSquares(int myArray[]);
+void printAsChars(int myArray[]);
 double getAverage(int myArray[]);
 int subtractAllValues(int myArray[]);
-//float invertAllValues(int myArray[] );
+float invertAllValues(int myArray[]);
 void printGroupMemberNames();
 
 int main(void) {
@@ -20,15 +20,50 @@ int main(void) {
 	int row[500], i;
 	for (i = 0; i < 500; i++)
 		row[i] = i;
-	subtractAllValues(row);
 
-	for (i = 0; i < 500; i++)
+	printf("The invertallvalues of all number is:\n", invertAllValues(row));
+	for (i = 0; i < 500; i++) {
 		printf("%d ", row[i]);
-
-	return 1;
+	}
+	printf("\nThe subtractallvalues of all number is: %d\n", subtractAllValues(row));
+	printf("The sumofsquares of all number is: %d\n", getSumOfSquares(row));
+	printf("The averange of all number is: %f\n", getAverage(row));
+	printf("The sum of all number is: %d\n", getSum(row));
+	printf("printAsChars function:");
+	printAsChars(row);
+	return 0;
 }
+float invertAllValues(int myArray[]) //invertAllValues
+{
+	int myBrray[500];
+	int i;
+	float s = 0;
+	for (i = 0; i<500; ++i)
+		myBrray[i] = myArray[499 - i];
+	for (i = 0; i<500; ++i)
+		myArray[i] = myBrray[i];
+	return 0;
+}
+void printAsChars(int myArray[]) { //printAsChars
+	int iCharLen = 500;
+	int i;
 
-int subtractAllValues(int myArray[])
+	for (i = 0; i<iCharLen; i++)
+	{
+		printf("%c", myArray[i]);
+	}
+}
+int getSumOfSquares(int myArray[]) //getSumSquares
+{
+	int i;
+	int sum = 0;
+	for (i = 0; i < 500; i++)
+	{
+		sum += myArray[i] * myArray[i];
+	}
+	return sum;
+}
+int subtractAllValues(int myArray[]) //SubtractAllValues
 {
 	int i;
 	//printf("subtractAllValues = ");
@@ -36,10 +71,8 @@ int subtractAllValues(int myArray[])
 		myArray[i] = -myArray[i];
 	}
 }
-
-// I just changed getAverage Function
-
-double getAverage(int myArray[]) {
+double getAverage(int myArray[]) //getAvg
+{
 	double total = 0;
 	int count = 0;
 	int i;
@@ -48,10 +81,15 @@ double getAverage(int myArray[]) {
 		count++;
 	}
 	return total / count;
-
 }
-
-
+int getSum(int myArray[]) //getSum
+{
+	int i;
+	int sum = 0;
+	for (i = 0; i<500; ++i)//iterate from 0 up to 499
+		sum += myArray[i];//increment sum by myArray[i]
+	return sum;
+}
 
 void printGroupMemberNames() {
 	printf("Dara Chheang\n");
@@ -61,4 +99,3 @@ void printGroupMemberNames() {
 	printf("Savorn Chea\n");
 	printf("Madalen Iv\n");
 }
-
