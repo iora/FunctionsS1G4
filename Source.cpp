c292e1e1d18958f93e2f1dff2802b2d1fed45f56
DaraChheang <dchhieng@zamanuniversity.edu.kh>
2017-05-04 11:01:30 +0700

    Dear Sir I wrote "invertAllValue" function. Does it right or not ?

--- Source.cpp
+++ Source.cpp
@@ -1,54 +1,51 @@
 #include <stdio.h>
 
-#include <stdlib.h>
+/*
+*	Please define your functions after main().
+*	You can import standard C / C++ libraries.
+*	Recommended: add description of your code.
+*/
 
-
-
-
-int getSum(int myArray[]);
-
+//int getSum(int myArray[]);
+//int getSumOfSquares(int myArray[]);
+//void printAsChars(int myArray[]);
+//double getAverage(int myArray[]);
+//int subtractAllValues(int myArray[]);
+float invertAllValues(int myArray[]);
+void printGroupMemberNames();
 
 int main(void) {
+	printGroupMemberNames();
 
-	//printGroupMemberNames();
-
-
-
-	int row[500];
-
-	for (int i = 0; i < 500; i++)
-
+	int row[500], i;
+	for (i = 0; i < 500; i++)
 		row[i] = i;
+	invertAllValues(row);
 
+	return 1;
+}
 
+float invertAllValues(int myArray[])
+{
+	int myBrray[500];
+	int i;
+	float s = 0;
+	for (i = 0; i<500; ++i)
+		myBrray[i] = myArray[499 - i];
 
-	printf("%d", getSum(row));
-
-
-
+	for (i = 0; i<500; ++i)
+		myArray[i] = myBrray[i];
 	return 0;
-
 }
 
 
 
-int getSum(int myArray[])
 
-{
-
-
-
-	int i;
-
-	int sum = 0;
-
-
-
-	for (i = 0; i<500; ++i)//iterate from 0 up to 499
-
-		sum += myArray[i];//increment sum by myArray[i]
-
-
-	return sum;
-
+void printGroupMemberNames() {
+	printf("Dara Chheang\n");
+	printf("Ratanaphea Chan\n");
+	printf("Chhorleang Horm\n");
+	printf("Sotheng Chheang\n");
+	printf("Savorn Chea\n");
+	printf("Madalen Iv\n");
 }
